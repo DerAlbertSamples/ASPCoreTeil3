a6bd12ca3699f97cb68409f66ca7a819d62ad572
Albert Weinert <info@der-albert.com>
2017-05-04 15:43:46 +0200

    fixed it

--- SecurityApp/Startup.cs
+++ SecurityApp/Startup.cs
@@ -34,7 +34,6 @@ namespace SecurityApp
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("Is16", builder =>
@@ -52,7 +51,7 @@ namespace SecurityApp
                 });
 
             });
-            services.AddSingleton<IAuthorizationHandler, InvoiceAuthorizationRequirementHandler>();
+            services.AddMvc();
             services.AddScoped<IInvoiceService, InvoiceService>();
         }
 
@@ -72,20 +71,7 @@ namespace SecurityApp
                 app.UseExceptionHandler("/Home/Error");
             }
 
-
-            app.UseCookiePolicy(new CookiePolicyOptions
-            {
-                Secure = CookieSecurePolicy.Always,
-                HttpOnly = HttpOnlyPolicy.Always,
-                OnAppendCookie = context =>
-                {
-                    context.CookieOptions.Expires = DateTimeOffset.Now.AddMinutes(10);
-                }
-            });
-
-
             app.UseStaticFiles();
-
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions()
             {
@@ -121,6 +107,7 @@ namespace SecurityApp
                     AutomaticChallenge = false
                 }
             );
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
